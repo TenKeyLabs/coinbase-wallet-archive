377bd80e6aeeac93a0f9da648a4651141a536f36
Dwayne Forde <dwayne@tenkeylabs.com>
2023-01-01 01:14:58 -0500

    action for checking and release

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+cd cwv_repo
+go build -o ../cwv main.go
+cd ..
+
+latest_version=`./cwv check`
+latest_release=`curl -s "https://github.com/osis/coinbase-wallet-archive/releases.atom" | grep -m2 -o '<title>[^<]*' | tail -n1 | grep -o '[^v]*$'`
+
+if [ $latest_release = $latest_version ]; then
+  exit 0
+fi
+
+./cwv download
+
+echo $latest_version
